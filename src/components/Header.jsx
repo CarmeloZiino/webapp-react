f9e643d611f25b8d03277cb1833f6a3f538c1a6d
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-03-17 14:21:45 +0100

    Add: logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+import cinemaKarme from "../assets/imgs/brandKarme.png";
 
 export default function Header() {
   return (
     <header>
       <nav className="navbar navbar-expand nav-bar-custom">
-        <div className="nav navbar-nav">
+        <div className="nav navbar-nav d-flex align-items-center">
+          <NavLink className="navbar-brand" to="/">
+            <img className="cinemaKarme" src={cinemaKarme} alt="brand" width={75}/>
+          </NavLink>
           <NavLink className="nav-item nav-link nav-route" to="/">
             Home
           </NavLink>
